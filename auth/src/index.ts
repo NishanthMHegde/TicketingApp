4c6d43f73116a5137e1dc380f6d37d25c36e9694
nishanthmhegde <=>
2021-03-15 21:22:09 +0530

    Added Error handling code with abstract class and async error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import {json} from 'body-parser';
+import {CurrentUserRouter} from './routes/current-user';
+import {SignUpRouter} from './routes/signup';
+import {SignInRouter} from './routes/signin';
+import {SignOutRouter} from './routes/signout';
+import {errorHandler} from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
+app.use(SignUpRouter);
+app.use(CurrentUserRouter);
+app.use(SignInRouter);
+app.use(SignOutRouter);
+app.use(errorHandler);
 
-app.get('/api/users/currentuser', (req, res)=>{
-	res.send('Current User');
-});
 
 app.listen(3000, ()=>{
 	console.log("Listening on port 3000");

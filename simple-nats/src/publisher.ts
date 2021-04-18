d1ba3f189f573b245f95bc75862d0f1485e1f04f
nishanthmhegde <=>
2021-04-18 12:50:10 +0530

    Added publisher class

--- simple-nats/src/publisher.ts
+++ simple-nats/src/publisher.ts
@@ -1,16 +1,14 @@
 import nats from 'node-nats-streaming';
-
+import {TicketCreatedPublisher} from './events/ticket-created-publisher';
 console.clear();
 const stan = nats.connect('ticketing', 'abc', {
 	url: 'http://localhost:4222'
 });
 
-stan.on('connect', ()=>{
+stan.on('connect', async ()=>{
 	console.log('Connected to NATS publisher');
 
-	const data = JSON.stringify({id:123, title: 'Dummy', price:10});
-	stan.publish('ticket:created', data, ()=>{
-		console.log('Data published');
-	});
-
+	const data = {id:'10', title: 'New Ticket', price:20};
+	const publisher = new TicketCreatedPublisher(stan);
+	await publisher.publish(data);
 });
